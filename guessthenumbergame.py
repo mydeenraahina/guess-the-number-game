8830a17d9dd8dd4f39c121346e930a498993913c
MydeenRaahina <145376500+mydeenraahina@users.noreply.github.com>
2023-11-10 12:16:02 -0500

    Update guessthenumbergame.py

--- guessthenumbergame.py
+++ guessthenumbergame.py
@@ -1,45 +1,46 @@
 import random
-def guess():
-    y=random.randint(1,10)
+def main_block():
+    computer_choice=random.randint(0,20)
     print("***guess the number!***")
-    guess=int(input("Enter your guessed number:"))
-    if guess==y:
+    print(f"Guess a number between [0 to 20]")
+    user_choice=int(input("Enter your guessed number:"))
+    if user_choice==  computer_choice:
         print('Congratulation you won in first try!!!')
     else:
         print('You have four more chance to won the game')
-        guess1=int(input("Enter your guessed number:"))
-        if guess1==y:
+        user_choice=int(input("Enter your guessed number:"))
+        if user_choice==  computer_choice:
               print("Congratulation you won in second try!!")
         else:
             print('you have three more chance to win the game')
-            guess2=input("enter your guessed number :")
-            if guess2==y:
+            user_choice=input("enter your guessed number :")
+            if user_choice== computer_choice:
                 print("Congratulation you won in third try!")
             else:
                 print('You have two more chance to won the game')
-                guess3=int(input("Enter your guessed number:"))
-                if guess1==y:
+                user_choice=int(input("Enter your guessed number:"))
+                if user_choice == computer_choice:
                     print("Congratulation you won in fouth try...")
                 else:
                     print('you have one more chance to win the game')
-                    guess4=input("enter your guessed number :")
-                    if guess4==y:
+                    user_choice=input("enter your guessed number :")
+                    if user_choice== computer_choice:
                         print("Congratulation you won in fifth try......")
                     else:
-                        print('sorry better luck next time!')
+                        print('computer won the game!')
 name=input("Enter your name:")
 print("welcome!",name,"our Number Guessing Game")
-choice=input("Do you want to start this game :")#type yes to start/no to exit
+choice=input(f"Do you want to start this game [yes/no]:")#type yes to start/no to exit
 if choice=="yes":
-    guess()
+    main_block()
+    while True:
+        a=input(f"Enter your choice[yes/no]")#type yes to continue/no to exit
+        if a=="yes":
+            main_block()
+        else:
+            print("--Exit!--")
+            break
 else:
-    print("type no to exit/yes to continue")
-while True:
-    a=input('enter your choice')#type yes to continue/no to exit
-    if a=="yes":
-        guess()
-    else:
-        print("--!--")
-        break;
-        
+    print("Exit!")
 
+        
